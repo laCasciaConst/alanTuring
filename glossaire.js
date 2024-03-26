8716d7b6a5e098c61fd8dd953ee281f9c12f9233
laCasciaConst <litashin.c@gmail.com>
2024-03-26 15:15:17 +0100

    first commit

---
+++ glossaire.js
@@ -0,0 +1,71 @@
+
+
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('header').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('header').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+    
+    lastScrollTop = st;
+}
+
+const buttons = document.querySelectorAll('.button');
+
+buttons.forEach(function(button, index) {
+  button.addEventListener('click', function(e) {
+    e.preventDefault();
+    
+    this.parentNode.classList.toggle('on');
+    
+    buttons.forEach(function(button2, index2) {
+      if ( index !== index2 ) {
+        button2.parentNode.classList.remove('on');
+      }
+    });
+  });
+});
+
+var acc = document.getElementsByClassName("button");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
